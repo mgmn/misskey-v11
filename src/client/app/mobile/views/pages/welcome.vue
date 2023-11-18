d4b8e0f90c6956005d79e1d6c0464733d70fb4bc
mogaminsk <mgmnjp@icloud.com>
2023-11-18 20:45:57 +0900

    hide explore link

--- src/client/app/mobile/views/pages/welcome.vue
+++ src/client/app/mobile/views/pages/welcome.vue
@@ -12,7 +12,7 @@
 			<div class="signin">
 				<a href="/signin" @click.prevent="signin()">{{ $t('@.signin') }}</a>
 			</div>
-			<div class="explore">
+			<div class="explore" v-if="meta && !(meta.authorizedProfileDirectory || meta.disableProfileDirectory)">
 				<router-link class="explore" to="/explore">{{ $t('@.explore') }}</router-link>
 			</div>
 		</div>
